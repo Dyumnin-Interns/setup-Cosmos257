e9f371fd25f0e87aa6d5ca756b4d8101e5283103
Cosmos257 <roopapramodan@gmail.com>
2025-07-06 17:41:12 +0400

    Update dut_wrapper.v

--- hdl/dut_wrapper.v
+++ hdl/dut_wrapper.v
@@ -1,1 +1,17 @@
+module dut_wrapper(
+  input wire a,
+	input wire b,
+	output wire y
+);
+  
+  dut dut(
+    .a(a),
+    .b(b),
+    .y(y)
+  );
 
+  inital begin
+    $dumpfile("waves.vcd");
+    $dumpvars;
+  end
+endmodule
